88409abad63822e4f1596eea2261c71e143e9b92
egjimenezg <egjimenezg@gmail.com>
2017-08-05 19:08:42 -0500

    Adding heapify function that ensures to keep the max heap property

--- Heap/heap.c
+++ Heap/heap.c
@@ -5,6 +5,9 @@
 #define right(position) ( (position<<1)+2 )
 #define SIZE 6
 
+void heapify(int*,int);
+void swap(int*,int*);
+
 int main(){
   /*The array represents the tree:  9 
                                   /   \
@@ -12,8 +15,36 @@ int main(){
                                /   \   /
                               49   10 1
   */
-                  //0|1| 2 |3 |4 |5
+                  
   int heap[SIZE] = {9,11,3,49,10,1};
-
+  //                0|1 |2| 3| 4|5 
   return 0;
+}
+
+void heapify(int* heap,int position){
+  int leftPosition = left(position);
+  int rightPosition = right(position);
+  int maxValuePosition;
+
+  if(leftPosition < SIZE && heap[leftPosition] > heap[position]){
+    maxValuePosition = leftPosition;
+  }
+  else{
+    maxValuePosition = position;
+  }
+
+  if(rightPosition < SIZE && heap[rightPosition] > heap[position]){
+    maxValuePosition = rightPosition;
+  }
+
+  if(maxValuePosition != position){
+    swap(&heap[position],&heap[maxValuePosition]);
+    heapify(heap,maxValuePosition);
+  }
+}
+
+void swap(int* positionFrom,int* positionTo){
+  int temporalPosition = (*positionFrom); 
+  (*positionFrom) = (*positionTo);
+  (*positionTo) = temporalPosition; 
 }
