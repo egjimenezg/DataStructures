b34300ee5a21cea8ace9c921c9f09cdecdc8e4bc
egjimenezg <egjimenezg@gmail.com>
2017-08-28 22:47:38 -0500

    Updating .gitignore and creating structure for the Heap

--- Heap/heap.h
+++ Heap/heap.h
@@ -1,8 +1,14 @@
 #define parent(position) ( position > 1 ? (position-1)>>1 : 0)
 #define left(position) ( (position<<1)+1 )
 #define right(position) ( (position<<1)+2 )
+#include <stdio.h>
 
-void heapify(int*,int,int);
+typedef struct Heap{
+  int* items; 
+  int heapSize;
+} Heap;
+
+void heapify(Heap*,int);
 void swap(int*,int*);
-void buildMaxHeap(int*,int);
+void buildMaxHeap(Heap*,int);
 void sort(int*,int);
