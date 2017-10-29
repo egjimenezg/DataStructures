1737d5716a4af7653ef4bf8410cc4d20f020d9dd
egjimenezg <egjimenezg@gmail.com>
2017-10-28 23:26:18 -0500

    Adding method that increments the value of an item in the heap at a specific position

--- PriorityQueue/priority_queue.c
+++ PriorityQueue/priority_queue.c
@@ -13,3 +13,17 @@ queue_entry extractMax(Heap* heap){
   return maxEntry;
 }
 
+void incrementValue(Heap *heap,long position,queue_entry value){
+  if(value < heap->items[position]){
+    perror("The new value is smaller than the old one");
+    return;
+  }
+
+  heap->items[position] = value;
+
+  while(position > 0 && heap->items[parent(position)] < heap->items[position]){
+    swap(&(heap->items[position]),&(heap->items[parent(position)]));
+    position = parent(position);
+  }
+
+}
--- PriorityQueue/priority_queue.h
+++ PriorityQueue/priority_queue.h
@@ -1,3 +1,5 @@
 #include "heap.h"
 typedef long queue_entry;
+
 queue_entry extractMax(Heap*);
+void incrementValue(Heap*,long,queue_entry);
